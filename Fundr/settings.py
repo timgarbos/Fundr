8da70f737ffdfd6026d412a20a22cf8ebc8abde3
unknown <Helgemis@.(none)>
2011-03-12 10:49:45 +0100

    Middleware folder added (also in settings.py)

--- Fundr/settings.py
+++ Fundr/settings.py
@@ -4,6 +4,7 @@ import os
 import sys
 PROJECT_ROOT = os.path.dirname(__file__)
 sys.path.insert(0, os.path.join(PROJECT_ROOT, "apps"))
+sys.path.insert(0, os.path.join(PROJECT_ROOT, "middleware"))
 print PROJECT_ROOT
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
